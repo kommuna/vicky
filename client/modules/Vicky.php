ae814cbcc1e7cdcf02ae965a813bf3a56a9b4bfe
Chewbacca96 <lebedev582@gmail.com>
2017-03-07 16:59:42 +0300

    [TL-3] JIRA to Slack mapping

--- client/modules/Vicky.php
+++ client/modules/Vicky.php
@@ -12,17 +12,42 @@ namespace Vicky\client\modules;
 
 class Vicky
 {
+    /**
+     * Project config
+     *
+     * @var
+     */
     private static $config;
 
+    /**
+     * Vicky constructor.
+     *
+     * @param $config
+     */
     public function __construct($config)
     {
         self::$config = $config;
     }
 
+    /**
+     * Return channel name by issue project name from config file
+     *
+     * @param $projectName issue project name
+     *
+     * @return null
+     */
     public static function getChannelByProject($projectName)
     {
         $jiraToSlackMapping = self::$config['jiraToSlackMapping'];
 
-        return empty($jiraToSlackMapping[$projectName]) ? '#events' : $jiraToSlackMapping[$projectName];
+        if (!empty($jiraToSlackMapping[$projectName])) {
+            $channel = $jiraToSlackMapping[$projectName];
+        } elseif (!empty($jiraToSlackMapping['EVERYTHINGELSE']) || $jiraToSlackMapping['EVERYTHINGELSE'] != 'false') {
+            $channel = $jiraToSlackMapping['EVERYTHINGELSE'];
+        } else {
+            $channel = null;
+        }
+
+        return $channel;
     }
 }

ccbd561a7ee32281937f39860dcc4d4bd6025e4b
Chewbacca96 <lebedev582@gmail.com>
2017-03-10 19:36:48 +0300

    [TL-3] Class for blockers issue notification

---
+++ src/modules/BlockersIssueFile.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * File review
+ *
+ * @credits https://github.com/kommuna
+ * @author  chewbacca@devadmin.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Vicky\src\modules;
+
+class BlockersIssueFile
+{
+    public function __construct()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+
+    public function delete()
+    {
+        
+    }
+}

812bdfe16b4b6b773684426212aed4e7e469ca69
Chewbacca96 <lebedev582@gmail.com>
2017-03-15 23:41:50 +0300

    [TL-15] Update

--- src/modules/BlockersIssueFile.php
+++ src/modules/BlockersIssueFile.php
@@ -25,17 +25,16 @@ class BlockersIssueFile
     public function get($pathToFile)
     {
         $data = file_get_contents($pathToFile);
-        return explode(' ', $data);
+        
+        return json_decode($data, true);
     }
 
     public function put($data)
     {
-        $date = (new DateTime())->add(new DateInterval('PT24H'));
+        $data['nextNotification'] = (new DateTime())->add(new DateInterval('PT24H'))->format('Y-m-d\TH:i:sP');
+        $data = json_encode($data);
 
-        return file_put_contents(
-            "{$this->pathToFolder}{$data->getIssue()->getKey()}",
-            print_r($data->getRawData(),true)." {$date->format('Y-m-d\TH:i:sP')}"
-        );
+        return file_put_contents("{$this->pathToFolder}{$data->getIssue()->getKey()}", $data);
     }
 
     public function delete()

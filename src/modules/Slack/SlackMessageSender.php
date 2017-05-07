23f88e80ee3c8991975a1801aaeae6bfef60d924
Chewbacca96 <lebedev582@gmail.com>
2017-05-07 15:04:09 +0300

    [TL-52] Comment fix

--- src/modules/Slack/SlackMessageSender.php
+++ src/modules/Slack/SlackMessageSender.php
@@ -9,7 +9,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Vicky\src\modules\Slack;
 
 use Maknz\Slack\Client;

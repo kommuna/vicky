651f8c603d8fbfbe024221bdb7ca3b7a4d3db230
Chewbacca96 <lebedev582@gmail.com>
2017-07-26 12:45:07 +0300

    [TL-63] Comments update

--- src/modules/Slack/Slack.php
+++ src/modules/Slack/Slack.php
@@ -2,6 +2,12 @@
 /**
  * Vicky module, that listening JIRA issue keys in slack channels
  * and sending notifications
+ *
+ * @credits https://github.com/kommuna
+ * @author  chewbacca@devadmin.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace kommuna\vicky\modules\Slack;
 
